e83549bb5a822ee02031b515eebbcbd4be53b67d
brandonapollo <brandonr@apollo.io>
2026-05-06 12:11:30 -0700

    Update apollo-io-cli to v0.5.2 (#4)

--- Formula/apollo-io-cli.rb
+++ Formula/apollo-io-cli.rb
@@ -1,22 +1,22 @@
 class ApolloIoCli < Formula
   desc "Apollo.io CLI for searching people, companies, and more"
   homepage "https://github.com/apolloio/apollo-io-cli"
-  version "0.5.1"
+  version "0.5.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/apolloio/apollo-io-cli/releases/download/v#{version}/apollo-macos-arm64"
-      sha256 "4d89afb45aeb8dc597c84452a509f58368d6ac2a8af07dce5a3846f207ac9521"
+      sha256 "65f98e1a677e9e97c279d81c7e6a1fd473f2db0b8eb307e6e530bfb31b0e5265"
     else
       url "https://github.com/apolloio/apollo-io-cli/releases/download/v#{version}/apollo-macos-x64"
-      sha256 "f0710a7eba4782ad914afa03d5ac2e0ffbe8b2b185d062cb9b46f2963174acd5"
+      sha256 "6d2fbff124c484d5ccb30e9caf6ce24e203a8d10160016f2fb7f2f60cbf436f0"
     end
   end
 
   on_linux do
     url "https://github.com/apolloio/apollo-io-cli/releases/download/v#{version}/apollo-linux-x64"
-    sha256 "22b417471281c087b497c1bb026b1caa3cab5b7939d0333901271ebb6caed1c0"
+    sha256 "104193ffe595097940fa2ce68b8241b4db561186cf87e68ebc4e9cf17935d64a"
   end
 
   def install
